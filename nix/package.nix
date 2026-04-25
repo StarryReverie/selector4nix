f6647e862a56f1a23cf87abcddf0de42af857797
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-04-25 13:18:47 +0800

    init: initialize repository

---
+++ nix/package.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "selector4nix";
+  version = "0.1.0";
+
+  src = lib.fileset.toSource {
+    root = ../.;
+    fileset = lib.fileset.unions [
+      ../Cargo.toml
+      ../Cargo.lock
+      ../src
+    ];
+  };
+
+  cargoLock = {
+    lockFile = ../Cargo.lock;
+  };
+
+  meta = {
+    description = "Nix substituter proxy with parallel cache queries and latency-aware selection";
+    homepage = "https://github.com/starryreverie/selector4nix";
+    mainProgram = "selector4nix";
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ starryreverie ];
+    platforms = lib.platforms.unix;
+  };
+}
