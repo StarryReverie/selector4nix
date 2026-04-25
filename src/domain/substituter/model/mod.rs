697b7728db94d0d036eebb6d68d628406a7cac9c
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-04-25 18:39:17 +0800

    feat(domain): add Priority value object

--- src/domain/substituter/model/mod.rs
+++ src/domain/substituter/model/mod.rs
@@ -1,3 +1,5 @@
+mod priority;
 mod url;
 
+pub use priority::{Priority, TryNewPriorityError};
 pub use url::{TryNewUrlError, Url};
---
+++ src/domain/substituter/model/priority.rs
@@ -0,0 +1,39 @@
+use std::num::NonZeroU32;
+
+use snafu::{OptionExt, Snafu};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Priority(NonZeroU32);
+
+impl Priority {
+    pub fn new(value: u32) -> Result<Self, TryNewPriorityError> {
+        Ok(Self(NonZeroU32::new(value).context(ZeroSnafu)?))
+    }
+
+    pub fn value(&self) -> u32 {
+        self.0.into()
+    }
+}
+
+#[derive(Snafu, Debug, Clone, Copy, PartialEq, Eq)]
+#[non_exhaustive]
+pub enum TryNewPriorityError {
+    #[snafu(display("priority should be positive"))]
+    Zero,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_succeeds() {
+        assert_eq!(Priority::new(1).unwrap().value(), 1);
+        assert_eq!(Priority::new(40).unwrap().value(), 40);
+    }
+
+    #[test]
+    fn new_fails_given_zero_value() {
+        assert!(matches!(Priority::new(0), Err(TryNewPriorityError::Zero)));
+    }
+}
