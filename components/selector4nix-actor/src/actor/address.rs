d5908c44d6bca040f8dd0c6b44bb3741c2da74da
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-04-27 19:17:37 +0800

    feat(selector4nix-actor): add actor-type-erased AnyAddress

--- components/selector4nix-actor/src/actor/address.rs
+++ components/selector4nix-actor/src/actor/address.rs
@@ -5,32 +5,92 @@ use crate::actor::{Actor, Message};
 
 #[derive(Debug)]
 pub struct Address<A: Actor> {
-    sender: Sender<Message<A::Request>>,
+    inner: AnyAddress<A::Request>,
 }
 
 impl<A: Actor> Address<A> {
     pub fn mock() -> (Self, Receiver<Message<A::Request>>) {
+        let (inner, receiver) = AnyAddress::mock();
+        (Self { inner }, receiver)
+    }
+
+    pub fn erased(self) -> AnyAddress<A::Request> {
+        self.inner
+    }
+
+    pub async fn tell(&self, request: A::Request) -> Result<(), TellError<A::Request>> {
+        self.inner.tell(request).await
+    }
+
+    pub fn try_tell(&self, request: A::Request) -> Result<(), TryTellError<A::Request>> {
+        self.inner.try_tell(request)
+    }
+
+    pub async fn shutdown(self) {
+        self.inner.shutdown().await
+    }
+
+    pub fn is_closed(&self) -> bool {
+        self.inner.is_closed()
+    }
+
+    pub fn is_same(&self, other: &Self) -> bool {
+        self.inner.is_same(&other.inner)
+    }
+}
+
+impl<A: Actor> Clone for Address<A> {
+    fn clone(&self) -> Self {
+        Self {
+            inner: self.inner.clone(),
+        }
+    }
+}
+
+impl<A: Actor> PartialEq for Address<A> {
+    fn eq(&self, other: &Self) -> bool {
+        self.is_same(other)
+    }
+}
+
+impl<A: Actor> Eq for Address<A> {}
+
+impl<A: Actor> From<Sender<Message<A::Request>>> for Address<A> {
+    fn from(sender: Sender<Message<A::Request>>) -> Self {
+        Self {
+            inner: AnyAddress::from(sender),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AnyAddress<R> {
+    sender: Sender<Message<R>>,
+}
+
+impl<R> AnyAddress<R> {
+    pub fn mock() -> (Self, Receiver<Message<R>>) {
         let (sender, receiver) = mpsc::channel(64);
         (sender.into(), receiver)
     }
 
-    pub async fn tell(&self, request: A::Request) -> Result<(), TellError<A::Request>> {
+    pub async fn tell(&self, request: R) -> Result<(), TellError<R>> {
         match self.sender.send(Message::Main(request)).await {
             Ok(()) => Ok(()),
             Err(SendError(Message::Main(request))) => Err(TellError(request)),
-            Err(_) => unreachable!(
-                "`Address::tell(..)` should not send messages other than `Message::Main(..)`"
-            ),
+            Err(_) => {
+                unreachable!("`tell(..)` should not send messages other than `Message::Main(..)`")
+            }
         }
     }
 
-    pub fn try_tell(&self, request: A::Request) -> Result<(), TryTellError<A::Request>> {
+    pub fn try_tell(&self, request: R) -> Result<(), TryTellError<R>> {
         match self.sender.try_send(Message::Main(request)) {
             Ok(()) => Ok(()),
             Err(TrySendError::Full(Message::Main(request))) => Err(TryTellError::Full(request)),
             Err(TrySendError::Closed(Message::Main(request))) => Err(TryTellError::Closed(request)),
             Err(_) => unreachable!(
-                "`Address::try_tell(..)` should not send messages other than `Message::Main(..)`"
+                "`try_tell(..)` should not send messages other than `Message::Main(..)`"
             ),
         }
     }
@@ -48,7 +108,7 @@ impl<A: Actor> Address<A> {
     }
 }
 
-impl<A: Actor> Clone for Address<A> {
+impl<R> Clone for AnyAddress<R> {
     fn clone(&self) -> Self {
         Self {
             sender: self.sender.clone(),
@@ -56,16 +116,16 @@ impl<A: Actor> Clone for Address<A> {
     }
 }
 
-impl<A: Actor> PartialEq for Address<A> {
+impl<R> PartialEq for AnyAddress<R> {
     fn eq(&self, other: &Self) -> bool {
         self.is_same(other)
     }
 }
 
-impl<A: Actor> Eq for Address<A> {}
+impl<R> Eq for AnyAddress<R> {}
 
-impl<A: Actor> From<Sender<Message<A::Request>>> for Address<A> {
-    fn from(sender: Sender<Message<A::Request>>) -> Self {
+impl<R> From<Sender<Message<R>>> for AnyAddress<R> {
+    fn from(sender: Sender<Message<R>>) -> Self {
         Self { sender }
     }
 }
--- components/selector4nix-actor/src/actor/mod.rs
+++ components/selector4nix-actor/src/actor/mod.rs
@@ -2,6 +2,6 @@ mod address;
 mod pre;
 mod runner;
 
-pub use address::{Address, TryTellError};
+pub use address::{Address, AnyAddress, TellError, TryTellError};
 pub use pre::{ActorPre, ActorPreBuilder};
 pub use runner::{Actor, Context, EmptyInternal, Message};
